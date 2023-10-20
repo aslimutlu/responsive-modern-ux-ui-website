c2e15d1910b25a0c558b07a02a750dcc0821ac99
Aslı Mutlu <asgul99@hotmail.com>
2023-10-20 23:45:28 +0300

    file structure created.

---
+++ responsive-modern-website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header  } from './containers'
+import {CTA, Brand, Navbar} from './components'
+
+function App() {
+  return (
+    <div>
+        <h1>GPT-3</h1>
+    </div>
+  )
+}
+
+export default App
